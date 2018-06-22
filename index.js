71d7dc52be96be932b7e4df0686178cf006880aa
Adam Tutaev <sendospro@mail.ru>
2018-06-22 18:01:03 +0300

    Upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var validate = require("./build/index.js");
+
+exports.isValid = async (emailAddress) => {
+	console.log(emailAddress);
+	try {
+		const email = new validate({ emailAddress });
+		const result = await email.check();
+		return result;
+	} catch (err) {
+		throw new Error(err);
+	}
+};
